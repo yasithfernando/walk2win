ffd63bbb8f47f99207f22c163295bde05ac5fde7
SinithH <sinithdj@gmail.com>
2023-09-26 15:08:23 +0530

    revery sync route auth

--- server/routes/syncRouter.js
+++ server/routes/syncRouter.js
@@ -5,10 +5,10 @@ const constData = require('../constants');
 let auth = require('./leaderBoardRouter');
 
 if (constData.contestOver) {
-    router.post('/api/v1/sync', auth.isAuthorized, syncController.syncSteps); 
-    router.post('/api/v1/syncmanual', auth.isAuthorized, syncController.syncStepsManual);
+    router.post('/api/v1/sync', syncController.syncSteps); 
+    router.post('/api/v1/syncmanual', syncController.syncStepsManual);
 }
-router.post("/api/v1/sync", auth.isAuthorized,  syncController.syncSteps); 
-router.get('/api/v1/playersync', auth.isAuthorized, syncController.playerSync);
-router.post('/api/v1/syncmanual', auth.isAuthorized, syncController.syncStepsManual);
+router.post("/api/v1/sync",  syncController.syncSteps); 
+router.get('/api/v1/playersync', syncController.playerSync);
+router.post('/api/v1/syncmanual', syncController.syncStepsManual);
 module.exports = router;
