f0cb50503a373bf94f90abcfbcce3fb0fc829cb9
SinithH <sinithdj@gmail.com>
2023-09-26 11:16:37 +0530

    add authorization to routes

--- server/routes/leaderBoardRouter.js
+++ server/routes/leaderBoardRouter.js
@@ -2,11 +2,21 @@ let express = require('express');
 let router = express.Router();
 let leaderBoardController = require('../controllers/leaderBoardController');
 
-router.get('/api/v1/leaderboard/topteams', leaderBoardController.topTeams);
-router.get('/api/v1/leaderboard/topteams/:teamId', leaderBoardController.topTeamPlayers);
-router.get('/api/v1/leaderboard/topplayers', leaderBoardController.topPlayers);
+router.get('/api/v1/leaderboard/topteams', isAuthorized, leaderBoardController.topTeams);
+router.get('/api/v1/leaderboard/topteams/:teamId', isAuthorized, leaderBoardController.topTeamPlayers);
+router.get('/api/v1/leaderboard/topplayers', isAuthorized, leaderBoardController.topPlayers);
 router.get('/api/v1/leaderboard/topplayers/:playerId', leaderBoardController.topPlayersData);
-router.get('/api/v1/leaderboard/topmaleplayers', leaderBoardController.topMalePlayers);
-router.get('/api/v1/leaderboard/topfemaleplayers', leaderBoardController.topFemalePlayers);
+router.get('/api/v1/leaderboard/topmaleplayers', isAuthorized, leaderBoardController.topMalePlayers);
+router.get('/api/v1/leaderboard/topfemaleplayers', isAuthorized, leaderBoardController.topFemalePlayers);
+
+function isAuthorized(req, res, next) {
+    const bearer = req.headers.authorization;
+    if (bearer === process.env.AUTH_TOKEN) {
+        next();
+    }
+    else {
+        res.sendStatus(401);
+    }
+  }
 
 module.exports = router;
