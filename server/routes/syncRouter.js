f14cf1f7c731f67192cd0ba478da8e11fbd9c10c
SinithH <sinithdj@gmail.com>
2023-09-26 12:42:45 +0530

    add authorization for sync

--- server/routes/syncRouter.js
+++ server/routes/syncRouter.js
@@ -2,12 +2,13 @@ let express = require('express');
 let router = express.Router();
 let syncController = require('../controllers/syncController');
 const constData = require('../constants');
+let auth = require('./leaderBoardRouter');
 
 if (constData.contestOver) {
-    router.post('/api/v1/sync', syncController.syncSteps); 
-    router.post('/api/v1/syncmanual', syncController.syncStepsManual);
+    router.post('/api/v1/sync', auth.isAuthorized, syncController.syncSteps); 
+    router.post('/api/v1/syncmanual', auth.isAuthorized, syncController.syncStepsManual);
 }
-router.post("/api/v1/sync", syncController.syncSteps); 
-router.get('/api/v1/playersync', syncController.playerSync);
-router.post('/api/v1/syncmanual', syncController.syncStepsManual);
+router.post("/api/v1/sync", auth.isAuthorized,  syncController.syncSteps); 
+router.get('/api/v1/playersync', auth.isAuthorized, syncController.playerSync);
+router.post('/api/v1/syncmanual', auth.isAuthorized, syncController.syncStepsManual);
 module.exports = router;
